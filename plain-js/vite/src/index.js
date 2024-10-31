3e8bbe6743de4c33862e576749f2a44d4e60271a
Clemens Anzmann <clemens.anzmann@googlemail.com>
2024-10-31 15:56:21 +1100

    remove deprecated nvl option

--- plain-js/vite/src/index.js
+++ plain-js/vite/src/index.js
@@ -5,7 +5,7 @@ const container = document.getElementById('app')
 const nodes = [{ id: '0', caption: 'graphs' }, { id: '1', caption: 'everywhere' }]
 const relationships = [{ from: '0', to: '1', id: '0-1', caption: 'are' }]
 
-const nvl = new NVL(container, nodes, relationships, { useWebGL: false, initialZoom: 2.6 })
+const nvl = new NVL(container, nodes, relationships, { initialZoom: 2.6 })
 
 const zoom = new ZoomInteraction(nvl)
 const pan = new PanInteraction(nvl)
