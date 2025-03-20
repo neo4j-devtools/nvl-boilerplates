316e3de275af4dcdd44bbf3e9590fc160bb18172
Clemens Anzmann <clemens.anzmann@googlemail.com>
2025-03-20 11:02:55 +1100

    add React app structure and update TypeScript configurations

--- react/typescript/vite/src/index.tsx
+++ react/typescript/vite/src/index.tsx
@@ -1,16 +1,11 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-import { InteractiveNvlWrapper } from '@neo4j-nvl/react'
+import { App } from "./App";
 
-createRoot(document.getElementById('app')).render(<div style={{ width: '100%', height: 500 }}>
-  <InteractiveNvlWrapper
-    nvlOptions={{ initialZoom: 2.6 }}
-    nodes={[{ id: '0', caption: 'graphs' }, { id: '1', caption: 'everywhere' }]}
-    rels={[{ from: '0', to: '1', id: '10', caption: 'are' }]}
-    mouseEventCallbacks={{
-      onZoom: true,
-      onPan: true
-    }}
-  />
-</div>)
+const container = document.getElementById("app");
+if (container) {
+  createRoot(container).render(<App />);
+} else {
+  console.error("No container found");
+}
--- react/typescript/webpack/src/index.tsx
+++ react/typescript/webpack/src/index.tsx
@@ -1,21 +1,11 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-import { InteractiveNvlWrapper } from '@neo4j-nvl/react'
+import { App } from "./App";
 
-const container = document.getElementById('app');
+const container = document.getElementById("app");
 if (container) {
-  createRoot(container).render(<div style={{ width: '100%', height: 500 }}>
-    <InteractiveNvlWrapper
-      nvlOptions={{ initialZoom: 2.6 }}
-      nodes={[{ id: '0', caption: 'graphs' }, { id: '1', caption: 'everywhere' }]}
-      rels={[{ from: '0', to: '1', id: '10', caption: 'are' }]}
-      mouseEventCallbacks={{
-        onZoom: true,
-        onPan: true
-      }}
-    />
-  </div>);
+  createRoot(container).render(<App />);
 } else {
-  console.error('No container found');
+  console.error("No container found");
 }
