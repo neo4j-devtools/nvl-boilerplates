7fab973ffa2cd0b31de07a187626e38db35154ef
Clemens Anzmann <clemens.anzmann@googlemail.com>
2026-05-12 15:32:24 +1000

    go zonless

--- angular/src/main.ts
+++ angular/src/main.ts
@@ -1,5 +1,7 @@
-import { bootstrapApplication } from "@angular/platform-browser";
-import { App } from "./app/app";
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideZonelessChangeDetection } from '@angular/core';
+import { App } from './app/app';
 
-bootstrapApplication(App)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+  providers: [provideZonelessChangeDetection()]
+}).catch((err) => console.error(err));
