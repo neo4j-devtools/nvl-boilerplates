f773fe7b5d73b857d2226eb6a20f5e39caf65896
Clemens Anzmann <clemens.anzmann@googlemail.com>
2026-06-23 10:48:02 +1000

    fix usage message to reflect correct script path

--- _housekeeping/serve-all.js
+++ _housekeeping/serve-all.js
@@ -7,7 +7,7 @@ const ROOT = path.join(__dirname, "..");
 
 function usage() {
   console.log(
-    "Usage: node serve-all-logs.js [--start-port=3000] [--folders=angular,nextjs,plain,react] [--verbose]"
+    "Usage: node _housekeeping/serve-all.js [--start-port=3000] [--folders=angular,nextjs,plain,react] [--verbose]"
   );
 }
 
